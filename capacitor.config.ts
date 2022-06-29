142d507d615252628c5678f61a5e3a7094207914
ElenaSidoroska11 <elena.sidoroska2@gmail.com>
2022-06-29 14:55:35 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SmartMeeteringApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
